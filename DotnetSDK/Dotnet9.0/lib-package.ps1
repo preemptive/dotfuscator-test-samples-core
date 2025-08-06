ee90866e6f20cbe51e13a411d3af619f3713595a
eduard.socea <eduard.socea@sembi.com>
2025-08-06 14:19:32 +0300

    updated packing

--- DotnetSDK/Dotnet9.0/lib-package.ps1
+++ DotnetSDK/Dotnet9.0/lib-package.ps1
@@ -6,21 +6,19 @@ Param(
 
 $scriptDirectory = $PSScriptRoot
 Write-Host "The script directory is: $scriptDirectory"
-
 if (-Not $NuGetSource) {
     $NuGetSource = $scriptDirectory + "\.packages"
 }
 
 if (-Not $Version) {
-    Write-Host -ForegroundColor Red "Please specifcy the version to publish"
-    Write-Host -ForegroundColor Cyan -NoNewLine "USAGE:"
-    Write-Host "lib-package.ps1 -version <version>"
-    Write-Host -ForegroundColor Yellow "Existing packages list below"
+    Write-Host -ForegroundColor Red "Please specifcy the version of the package"
+    Write-Host -ForegroundColor DarkYellow "USAGE: lib-package.ps1 -version <version>"
+    Write-Host -ForegroundColor DarkYellow "See below the existing packages"
     nuget list -source $NuGetSource
     exit -1
 }
 
 dotnet build .\Samples.Core.Lib\Samples.Core.Lib.csproj -c Release
-Write-Host -ForegroundColor Yellow "Packing and publishing package"
+Write-Host -ForegroundColor Green "Packing and publishing package"
 dotnet pack .\Samples.Core.Lib\Samples.Core.Lib.csproj -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -c Release /p:Version=$version
 dotnet nuget push .\Samples.Core.Lib\bin\release\PreEmptive.Dotfuscator.Samples.Core.Lib.$Version.nupkg -s $NuGetSource
