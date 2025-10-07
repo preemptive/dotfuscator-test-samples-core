1aed96b38640dcbeefd227c4377821c9210ac94e
Vijay Kumar <vijay.kumar@CELSYSWTC.IN>
2025-10-07 20:30:19 +0530

    PRE-2995 changed the string from concatenation to long string

--- DotnetSDK/Dotnet9.0/Samples.Core/Services/StepProcessors/LongStringStepProcessor.cs
+++ DotnetSDK/Dotnet9.0/Samples.Core/Services/StepProcessors/LongStringStepProcessor.cs
@@ -9,12 +9,12 @@ namespace PreEmptive.Dotfuscator.Samples.Core.Services.StepProcessors
     public class LongStringStepProcessor : StepProcessorBase
     {
         public static readonly string LongStringTest =
-           "This is a very long string for encryption testing. " +
-           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
-           "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
-           "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. " +
-           "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
-           "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+          @"This is a very long string for encryption testing.
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+           Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
         protected override async Task<StepResult> ExecuteInternalAsync(CancellationToken cancellationToken = default)
         {
