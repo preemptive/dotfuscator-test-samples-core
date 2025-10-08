6fa7dffcdf868656d1e91ff140f232fb645b98ea
vijaycel123 <vijay.kumar@celestialsys.com>
2025-10-08 19:25:22 +0530

    PRE-2955 Implement long string in the Core samples (#14)
    * PRE-2955 Implemented long string
    
    * PRE-2995 changed the string from concatenation to long string
    
    ---------
    
    Co-authored-by: Vijay Kumar <vijay.kumar@CELSYSWTC.IN>

---
+++ DotnetSDK/Dotnet9.0/Samples.Core/Services/StepProcessors/LongStringStepProcessor.cs
@@ -0,0 +1,24 @@
+﻿using PreEmptive.Dotfuscator.Samples.Core.Abstractions;
+using PreEmptive.Dotfuscator.Samples.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PreEmptive.Dotfuscator.Samples.Core.Services.StepProcessors
+{
+    public class LongStringStepProcessor : StepProcessorBase
+    {
+        public static readonly string LongStringTest =
+          @"This is a very long string for encryption testing.
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+           Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+        protected override async Task<StepResult> ExecuteInternalAsync(CancellationToken cancellationToken = default)
+        {
+            return StepResult.Success(message: $"\n Long String : \n{LongStringTest}\n");
+        }
+    }
+}
