fea75f90386a1335d08dcf63cb18b718041d9567
Dragos Cucu <dragos.cucu@sembi.com>
2026-02-27 00:44:08 +0200

    PRE-3784 Add RedundantInterfaceRemovalStepProcessor, enable remove_redundant_interfaces property in ConsoleApp

---
+++ DotnetSDK/DotNetFramework4.7.2/Samples.Core/Abstractions/RedundantNestedInterfaces.cs
@@ -0,0 +1,10 @@
+﻿namespace PreEmptive.Dotfuscator.Samples.Core.Abstractions
+{
+    public interface IBase1<T>
+    {
+        T GetSampleValue();
+    }
+    public interface IBase2<T1, T2> : IBase1<T1> { }
+    public interface ISample1<T> : IBase1<T> { }
+    public interface ISample2<T1, T2> : ISample1<T1>, IBase2<T1, T2>, IBase1<T1> { }
+}
